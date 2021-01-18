ace693a2353dd69e0d6519b784b2852afa72b277
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-19 00:01:49 +0200

    header  component setup

--- gmail/src/App.js
+++ gmail/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h2>Welcome to gmail</h2>
+      <Header />
     </div>
   );
 }
