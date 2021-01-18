9bdc411411b0b72ebe6c82ed2386ad48030f0194
kabagema <35719135+kabagema@users.noreply.github.com>
2021-01-18 23:16:35 +0200

    set up of the project

---
+++ gmail/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h2>Welcome to gmail</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+//  react form book for validation
+//  google AuthenticatorAssertionResponse
+//  css responsive
+//  redux
